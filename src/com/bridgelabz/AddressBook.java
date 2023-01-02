960e655ea391eaaf99d5c9b770edfacf19085a88
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-03 00:22:54 +0530

    [Kavyasree J ganesh] Address book city and state dictionary

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,11 +1,14 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBook {
 
     ArrayList<ContactPerson> contactList = new ArrayList<>();
+    HashMap<String,ArrayList<ContactPerson>> contactsByCity = new HashMap<>();
+    HashMap<String,ArrayList<ContactPerson>> contactsByState = new HashMap<>();
 
     public void addContact() {
         ContactPerson contactPerson = new ContactPerson();
@@ -26,6 +29,22 @@ public class AddressBook {
         System.out.println("Enter the Mobile no : ");
         contactPerson.setPhoneNo(sc.next());
         contactList.add(contactPerson);
+
+        if(contactsByCity.containsKey(contactPerson.getCity())){
+            ArrayList<ContactPerson> contacts = contactsByCity.get(contactPerson.getCity());
+            contacts.add(contactPerson);
+            contactsByCity.put(contactPerson.getCity(), contacts);
+        } else{
+            contactsByCity.put(contactPerson.getCity(), new ArrayList<>(contactList));
+        }
+
+        if(contactsByState.containsKey(contactPerson.getState())){
+            ArrayList<ContactPerson> contacts = contactsByState.get(contactPerson.getState());
+            contacts.add(contactPerson);
+            contactsByCity.put(contactPerson.getState(), contacts);
+        } else{
+            contactsByState.put(contactPerson.getState(), new ArrayList<>(contactList));
+        }
     }
 
     @Override
@@ -41,6 +60,8 @@ public class AddressBook {
         String name = sc.next();
         for (ContactPerson contactPerson : contactList) {
             if (name.equals(contactPerson.getName())) {
+                String city = contactPerson.getCity();
+                String state = contactPerson.getState();
                 System.out.println("Set Details");
                 System.out.print("Enter first name:");
                 contactPerson.setName(sc.next());
@@ -56,6 +77,23 @@ public class AddressBook {
                 contactPerson.setZipCode(sc.next());
                 System.out.println("Enter the Mobile no : ");
                 contactPerson.setPhoneNo(sc.next());
+                if(contactsByState.containsKey(state)){
+                    for (int i=0; i<contactsByState.get(state).size();i++){
+                        if(name.equals(contactsByState.get(state).get(i).getName())){
+                            contactsByState.get(state).remove(i);
+                            contactsByState.get(state).add(contactPerson);
+                        }
+                    }
+                }
+
+                if(contactsByCity.containsKey(city)){
+                    for (int i=0; i<contactsByCity.get(city).size();i++){
+                        if(name.equals(contactsByCity.get(city).get(i).getName())){
+                            contactsByCity.get(city).remove(i);
+                            contactsByCity.get(city).add(contactPerson);
+                        }
+                    }
+                }
                 break;
             }
         }
@@ -70,6 +108,16 @@ public class AddressBook {
         for (ContactPerson contactPerson : contactList) {
             if (name.equals(contactPerson.getName())) {
                 contactList.remove(contactPerson);
+                if(contactsByState.containsKey(contactPerson.getState())){
+                    ArrayList<ContactPerson> contacts = contactsByState.get(contactPerson.getState());
+                    contacts = new ArrayList<>(contacts.stream().filter(x-> x.getName() != contactPerson.getName()).toList());
+                    contactsByState.put(contactPerson.getState(), contacts);
+                }
+                if(contactsByState.containsKey(contactPerson.getCity())){
+                    ArrayList<ContactPerson> contacts = contactsByCity.get(contactPerson.getCity());
+                    contacts = new ArrayList<>(contacts.stream().filter(x-> x.getName() != contactPerson.getName()).toList());
+                    contactsByCity.put(contactPerson.getCity(), contacts);
+                }
                 break;
             }
 
@@ -79,7 +127,7 @@ public class AddressBook {
         Scanner scanner = new Scanner(System.in);
         int opration;
         do {
-            System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3 EDIT \n4 Delete \n5. EXIT ");
+            System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3 EDIT \n4 Delete \n 5 Display by city \n 6 Display by state \n7. EXIT ");
             System.out.println("Enter the Operation Number");
             opration = scanner.nextInt();
             switch (opration) {
@@ -96,12 +144,22 @@ public class AddressBook {
                     deleteContact();
                     break;
                 case 5:
+                    System.out.println("Enter the  city");
+                    String city = scanner.next();
+                    System.out.println(contactsByCity.get(city));
+                    break;
+                case 6:
+                    System.out.println("Enter the  state");
+                    String state = scanner.next();
+                    System.out.println(contactsByState.get(state));
+                    break;
+                case 7:
                     System.out.println("Exiting");
                     break;
                 default:
                     System.out.println("Enter The Wrong Opration Number");
             }
-        } while (opration != 5);
+        } while (opration != 7);
     }
 
 }
